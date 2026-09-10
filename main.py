84d4c6844cc8d9d424a246a3c5e1cc6d7b486069
Norvent <gordlab@mail.ru>
2026-09-10 10:08:56 +1000

    chore: improve syntax

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from retrieve_params import main as retrieve_params
+from retrieve_text import main as retrieve_text
 
 
-def main():
-    retrieve_params()
-    ...
+def main(path: str, data_to_extract: str):
+    text = retrieve_text(path=path)
+    retrieve_params(document=text, data_to_extract=data_to_extract)
+    print('ЗАВЕРШЕНО ВСЕ!')
 
 
 if __name__ == "__main__":
-    main()
+    doc_path = input('Введите путь к документу: ')
+    doc_params = input("Перечислите ключевые параметры через запятую: ")
+
+    main(path=doc_path, data_to_extract=doc_params)
